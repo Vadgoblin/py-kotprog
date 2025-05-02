b2074fda767864dc48b4f6efd90d518c4d5e9a7f
vadgoblin@arch <vadgoblin@github.com>
2025-05-02 22:32:25 +0200

    refactor

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import pygame
 from config import Config
 from game import Game
 
-config = Config()
 
-pygame.init()
-X = config.game["width"]
-Y = config.game["height"]
+_screen = None
 
-screen = pygame.display.set_mode((X, Y))
-pygame.display.set_caption('PvZ paint edition')
+def main():
+    _initialize_screen()
+    game = Game(_screen)
+    game.start()
 
-game = Game(screen)
-game.start()
+def _initialize_screen():
+    global _screen
+    config = Config()
+    pygame.init()
+    x = config.game["width"]
+    y = config.game["height"]
+
+    _screen = pygame.display.set_mode((x, y))
+    pygame.display.set_caption('PvZ paint edition')
+
+if __name__ == "__main__":
+    main()
