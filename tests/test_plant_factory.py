4302a5722f6fb764e66d7b600c93483c44b7085e
vadgoblin@arch <vadgoblin@github.com>
2025-05-05 18:53:57 +0200

    add plant factory test

---
+++ tests/test_plant_factory.py
@@ -0,0 +1,24 @@
+import unittest
+from unittest.mock import Mock
+from src.pvz.game.plant.plantFactory import plant_factory
+from src.pvz.game.plant.plants import peashooter,sunflower,wallnut,cherrybomb
+
+class TestPlantFactory(unittest.TestCase):
+    def test_peashooter(self):
+        plant = plant_factory(Mock(),"peashooter",0,0)
+        self.assertIsInstance(plant,peashooter.Peashooter)
+
+    def test_wallnut(self):
+        plant = plant_factory(Mock(),"wallnut",0,0)
+        self.assertIsInstance(plant,wallnut.Wallnut)
+
+    def test_sunflower(self):
+        plant = plant_factory(Mock(),"sunflower",0,0)
+        self.assertIsInstance(plant,sunflower.Sunflower)
+
+    def test_cherrybomb(self):
+        plant = plant_factory(Mock(),"cherrybomb",0,0)
+        self.assertIsInstance(plant,cherrybomb.Cherrybomb)
+
+    def test_invalid(self):
+        self.assertRaises(Exception,plant_factory,Mock(),"asd",0,0)
